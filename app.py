536cad93f5a6ececb4358d52ba4bd58dd94625e3
rhaws87 <rhaws87@comcast.net>
2019-08-20 22:55:17 -0500

    Comments added

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 
 
 #################################################
-# Date Definitions
+# Date Definitions for Precipitation & Temperature
 last_date = (session.query(Measurement.date).order_by(Measurement.date.desc()).first())
 latest_date = list(np.ravel(last_date))[0]
 
@@ -66,18 +66,19 @@ def welcome():
 
 @app.route("/api/v1.0/station")
 def stations():
+    #basic list of stations
     results = session.query(Station.name).all()
     all_stations = list(np.ravel(results))
     return jsonify(all_stations)
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    
+    #define columns for precipitation, use calculations from above to define range
     inches = (session.query(Measurement.date, Measurement.prcp, Measurement.station)
                       .filter(Measurement.date > year_prior)
                       .order_by(Measurement.date)
                       .all())
-    
+    #create list, loop & append date range & precipitation
     precip_data = []
     for INCH in inches:
         precip_dict = {INCH.date: INCH.prcp, "Station": INCH.station}
@@ -87,12 +88,12 @@ def precipitation():
 
 @app.route("/api/v1.0/temperature")
 def temperature():
-
+#define columns for temperature, use calculations from above to define range
     farenheit = (session.query(Measurement.date, Measurement.tobs, Measurement.station)
                       .filter(Measurement.date > year_prior)
                       .order_by(Measurement.date)
                       .all())
-
+#create list, loop & append date range & precipitation
     temp_data = []
     for temp in farenheit:
         temp_dict = {temp.date: temp.tobs, "Station": temp.station}
@@ -102,13 +103,15 @@ def temperature():
        
 @app.route("/api/v1.0/datesearch/<StartDate>")
 def start(StartDate):
+    #call table columns
     sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-                      
+    #query data based on date range (formatted date, just like python file)                  
     temp_date =  (session.query(*sel)
                        .filter(func.strftime("%Y-%m-%d", Measurement.date) >= StartDate)
                        .group_by(Measurement.date)
                        .all())
-    dates = []
+    #create a list, loop & append the columns for the date range defined from the JSON requst above
+        dates = []
     for time in temp_date:
         date_dict = {}
         date_dict["Date"] = time[0]
@@ -121,13 +124,15 @@ def start(StartDate):
 
 @app.route("/api/v1.0/datesearch/<StartDate>/<EndDate>")
 def start_end(StartDate,EndDate):
+    #call table columns
     sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-                      
+     #query data based on date range (formatted data, just like python file)                 
     temp_date_range =  (session.query(*sel)
                        .filter(func.strftime("%Y-%m-%d", Measurement.date) >= StartDate)
                        .filter(func.strftime("%Y-%m-%d", Measurement.date) <= EndDate)
                        .group_by(Measurement.date)
                        .all())
+    #create a list, loop & append the columns for the date range defined in the JSON request
     dates = []
     for time_range in temp_date_range:
         date_dict = {}
